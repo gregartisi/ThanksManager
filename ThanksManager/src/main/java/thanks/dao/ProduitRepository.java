de41cd44251c8d129dc855dcd0b1df5dc12f9c65
gregartisi <gregoryartisi@hotmail.com>
2018-07-29 23:31:48 +0200

    search by type
    possibility to search by type and by keyword, and both!

--- ThanksManager/src/main/java/thanks/dao/ProduitRepository.java
+++ ThanksManager/src/main/java/thanks/dao/ProduitRepository.java
@@ -2,6 +2,7 @@ package thanks.dao;
 import java.util.List;
 
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -19,6 +20,9 @@ public interface ProduitRepository extends JpaRepository<Produit, Long> {
 	
 	@Query("select p from Produit p where p.name like:x")
 	public Page<Produit> searchProduit(@Param(value="x")String keyword,Pageable pages);
+	
+	@Query("select p from Produit p where p.typeProduit=:tp and p.name like:x")
+	public Page<Produit> searchProduitByType(@Param(value="x")String keyword,@Param(value="tp")long tp, Pageable pages);
 	
 
 }
